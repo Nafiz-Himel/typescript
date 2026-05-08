2251ef5fc2d7f6a5f221fbe533f89422590df687
Nafiz Himel <nafizhimel7@gmail.com>
2026-05-08 12:52:12 +0600

    funciton-added

--- src/function.ts
+++ src/function.ts
@@ -1,1 +1,31 @@
 //function
+//arrow,normal
+
+function addNormal(num1:number,num2:number):number{
+    return num1+num2;
+}
+
+// addNormal(2,"2");
+
+
+const addArrow = (num1:number,num2:number):number => 
+    num1+num2;
+
+// addArrow(2,"2");
+addArrow(2,2);
+
+//object => function => method
+const poorUser ={
+    name: "mezba",
+    balance: 0,
+
+    addBalance(value: number):number{
+        const totalBalance = this.balance + value;
+        return totalBalance;
+    }
+}
+
+poorUser.addBalance(100000);
+
+const arr: number[] =[1,2,4];
+const sqrArray = arr.map((elem:number):number => elem*elem);
