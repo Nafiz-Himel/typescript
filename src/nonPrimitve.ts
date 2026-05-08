7bef2062b34f0010b751f254692a701cc31a9a83
Nafiz Himel <nafizhimel7@gmail.com>
2026-05-08 11:59:22 +0600

    learning-typescript

---
+++ src/nonPrimitve.ts
@@ -0,0 +1,13 @@
+//array, object
+//ts - tuple
+
+let bazarList: string[] = ["rice", "dal", "oil"];
+
+// bazarList.push(12);
+
+
+let mixedArr:(string|number)[] = ["eggs",12,"milk",1,"sugar",2];
+// mixedArr.push(true);
+let mixedArrr = ["eggs",12,"milk",1,"sugar",2];
+
+
---
+++ src/primitive.ts
@@ -0,0 +1,26 @@
+//js - ts
+//string,number,boolean,undefined,null
+
+
+//ts:never,unknown,void
+
+let userName: string
+// userName = 12;
+userName = "12";
+
+let userId = 123;
+// userId = himel;
+userId = 453;
+
+// userName.toFixed();
+userId.toFixed();
+
+let isLoggedIn: boolean;
+isLoggedIn = true;
+
+let y: undefined = undefined;
+// y = 5;
+let x = undefined; //any type
+x = 5;
+x = "5";
+
